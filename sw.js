dabb8d0fa2821427bd11118837d7a672b7be9038
Oliver <oescobarh1@miumg.edu.gt>
2026-04-22 19:13:08 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'gym-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&family=DM+Mono:wght@400;500&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
